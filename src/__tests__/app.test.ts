e1536daccfefd08cd72f1133303af66fee9470dd
Omar Lengua <omarns21@gmail.com>
2024-12-04 13:00:16 +0100

    Add test

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,13 @@
+import sequelize from '../config/sequelize';
+
+describe('Database Connection', () => {
+  it('should connect to the database successfully', async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+      throw error;
+    }
+  });
+});
